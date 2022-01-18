68d95b3b7b5a7d0b97a955b5d5e1093a23e2123a
Himani <himani9.engg@gmail.com>
2022-01-18 19:31:54 +0530

    used useState to update index of clicked item

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
   const onTitleClick = (index) => {
-    console.log("title clicked", index);
+    setActiveIndex(index);
   };
   const renderedItems = items.map((item, index) => {
     return (
@@ -17,7 +18,12 @@ const Accordion = ({ items }) => {
       </React.Fragment>
     );
   });
-  return <div className="ui styled accordion">{renderedItems}</div>;
+  return (
+    <div className="ui styled accordion">
+      {renderedItems}
+      <h1>{activeIndex}</h1>
+    </div>
+  );
 };
 
 export default Accordion;
