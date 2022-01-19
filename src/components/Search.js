102f3c03ebaf8b1c805eb2046c459b594ed242fc
Himani <himani9.engg@gmail.com>
2022-01-19 11:27:58 +0530

    async code in useEffect

---
+++ src/components/Search.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+const Search = () => {
+  const [term, setTerm] = useState("");
+  useEffect(() => {
+    const search = async () => {
+      await axios.get("sdcs");
+    };
+    search();
+  }, [term]);
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Search;
