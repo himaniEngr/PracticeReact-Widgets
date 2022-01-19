8a4a6b3cf0c4058ea4077edb675320c27aa20b3b
Himani <himani9.engg@gmail.com>
2022-01-19 19:49:53 +0530

    searching on initial render

--- src/components/Search.js
+++ src/components/Search.js
@@ -18,11 +18,18 @@ const Search = () => {
       });
       setResults(data.query.search);
     };
-    const timeoutId = setTimeout(() => {
-      if (term) {
-        search();
-      }
-    }, 500);
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 1000);
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
   }, [term]);
   const renderedResults = results.map((result) => {
     return (
