a85fe5fabaa45ac10eb4967b9597ce86cc995355
Himani <himani9.engg@gmail.com>
2022-01-20 12:56:32 +0530

    scaffolding the drop-down

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 const items = [
   {
     title: "What is React?",
@@ -15,12 +16,26 @@ const items = [
     content: "You use react by creating components",
   },
 ];
+const options = [
+  {
+    label: "The color red",
+    value: "red",
+  },
+  {
+    label: "The color green",
+    value: "green",
+  },
+  {
+    label: "The color blue",
+    value: "blue",
+  },
+];
 
 export default () => {
   return (
     <div>
       {/* <Accordion items={items} /> */}
-      <Search />
+      <Dropdown options={options} />
     </div>
   );
 };
