a358fd3e7d01ec73c8233b45788d652bb955b31d
Himani <himani9.engg@gmail.com>
2022-01-21 16:51:45 +0530

    basic component routing

--- src/App.js
+++ src/App.js
@@ -53,10 +53,34 @@ const options = [
 //     </div>
 //   );
 // };
+const showAccordion = () => {
+  if (window.location.pathname === "/") {
+    return <Accordion items={items} />;
+  }
+};
+const showList = () => {
+  if (window.location.pathname === "/list") {
+    return <Search />;
+  }
+};
+const showDropdown = () => {
+  if (window.location.pathname === "/dropdown") {
+    return <Dropdown />;
+  }
+};
+const showTranslate = () => {
+  if (window.location.pathname === "/translate") {
+    return <Translate />;
+  }
+};
+
 export default () => {
   return (
     <div>
-      <Translate />
+      {showAccordion()}
+      {showList()}
+      {showTranslate()}
+      {showDropdown()}
     </div>
   );
 };
